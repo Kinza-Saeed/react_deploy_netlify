70c57cc1bba75eee64663d01a4a8f8df91eade9a
Kinza-Saeed <kinzasaeed1005@gmail.com>
2024-02-26 01:40:08 -0800

    first

---
+++ src/About.js
@@ -0,0 +1,11 @@
+
+const About = () => {
+  return (
+    <main className="About">
+      <h2> About </h2>
+      <p style={{marginTop:"1rem"}}> This blog post is a part of learn react series.</p>
+    </main>
+  )
+}
+
+export default About
